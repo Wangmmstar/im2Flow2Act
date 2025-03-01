2857dd026acbaadca27fb9edf686598ee092c6b1
Mengjun Wang <mwang5@c36a-s1.ufhpc>
2025-03-01 17:41:47 -0500

    comments

--- scripts/data_script/viz_all.py
+++ scripts/data_script/viz_all.py
@@ -1,3 +1,5 @@
+# visualizing optical flows from a dataset.  It does this by running another script, viz_point_tracking.py, multiple times using subprocess.Popen().
+
 import os
 import subprocess
 
@@ -10,28 +12,35 @@ def main():
         f"{dev_path}/im2Flow2Act/data/simulated_play/deformable",
     ]
     print(viz_pathes)
+
+    """
+    -1: Use precomputed moving masks in the dataset (recommended for simulated data).
+    >0: Visualize keypoints that moved more than the specified threshold.
+    0: Used for real-world data with bounding boxes.
+    """
+
     # viz_thresholds = [0]  # use it for viz_bbox for realworld dataset
-    viz_thresholds = [-1] * len(viz_pathes)  # use it for viz_sam for sim dataset
+    viz_thresholds = [-1] * len(viz_pathes)  # use it for viz_sam for sim dataset  return [X,X,X] three values loop three data paths
     processes = []
     for v in viz_pathes:
         for thresh in viz_thresholds:
-            process = subprocess.Popen(
+            process = subprocess.Popen( #calls viz_point_tracking.py using subprocess.Popen(), passing several arguments, run several processes in parallel, each file as separate process
                 [
                     "python",
-                    "viz_point_tracking.py",
+                    "viz_point_tracking.py", # ccall this file
                     "--viz_num",
-                    str(20),
+                    str(20),   # Process 20 data samples.
                     "--data_buffer_path",
                     v,  # Split into two separate strings
                     "--viz_threshold",
                     str(thresh),  # Also split, and convert thresh to string
                     "--viz_num_point",
-                    str(-1),
+                    str(-1), #Visualizes all keypoints.
                     # "--draw_line",
                     "--viz_offset",
                     str(0),
                     "--viz_save_path",
-                    f"{dev_path}/im2Flow2Act/experiment/visual_pt",
+                    f"{dev_path}/im2Flow2Act/experiment/visual_pt", # Saves results to experiment/visual_pt/.
                     "--viz_sam",
                     # "--viz_bbox",
                 ],
--- scripts/data_script/viz_point_tracking.py
+++ scripts/data_script/viz_point_tracking.py
@@ -1,3 +1,6 @@
+# visualizes keypoint tracking data stored in a Zarr file format
+#  processes sequences from a dataset, extracts keypoint movements, and generates visualizations as GIFs.
+
 import argparse
 import os
 
@@ -20,17 +23,22 @@ def main():
     # Create the parser
     parser = argparse.ArgumentParser()
     # Add arguments
-    parser.add_argument("--data_buffer_path", type=str)
-    parser.add_argument("--viz_num", type=int, default=1)
+    parser.add_argument("--data_buffer_path", type=str)  # zarr dataset path
+    parser.add_argument("--viz_num", type=int, default=1) # Number of episodes to visualize.
     parser.add_argument("--viz_threshold", type=float, default=0)
     parser.add_argument("--viz_offset", type=int, default=0)
-    parser.add_argument("--viz_num_point", type=int, default=-1)
+    parser.add_argument("--viz_num_point", type=int, default=-1) #Number of keypoints to visualize (-1 means all).
     parser.add_argument("--viz_save_path", type=str)
-    parser.add_argument("--draw_line", action="store_true")
-    parser.add_argument("--viz_sam", action="store_true")
-    parser.add_argument("--viz_bbox", action="store_true")
+    parser.add_argument("--draw_line", action="store_true")  #If set, draws lines connecting keypoints.
+    parser.add_argument("--viz_sam", action="store_true") #Uses precomputed moving masks for visualization.
+    parser.add_argument("--viz_bbox", action="store_true")  #Uses bounding box tracking instead of general keypoints.
+
 
     # Parse the arguments
+    """
+    Extracts the name of the dataset (embodiment_name).
+    Creates a folder for saving results (embodiment_save_path)
+    """
     args = parser.parse_args()
     data_buffer_path = args.data_buffer_path
     embodiment_name = get_embodiment_name(data_buffer_path)
@@ -44,8 +52,11 @@ def main():
     print(f"Opening Zarr file at: {data_buffer_path}")
 
     buffer = zarr.open(data_buffer_path)
+
+
     for i in tqdm(range(args.viz_num)):
         try:
+            # extract keypoint tracking sequence from the dataset
             if args.viz_bbox:
                 tracking_point_sequence = buffer[
                     f"episode_{i+args.viz_offset}/bbox_point_tracking_sequence"
@@ -58,7 +69,11 @@ def main():
                 tracking_point_sequence = buffer[
                     f"episode_{i+args.viz_offset}/point_tracking_sequence"
                 ][:]
+            
+            #rgb frames
             frames = buffer[f"episode_{i+args.viz_offset}/rgb_arr"][:]
+
+            # filter the keywoints based on movement --- threshold  ---> pixel number
             if args.viz_threshold >= 0:
                 moving_mask = (
                     max_distance_moved(tracking_point_sequence) > args.viz_threshold
@@ -73,10 +88,11 @@ def main():
             viz_seq = tracking_point_sequence[moving_mask]
             if args.viz_num_point != -1:
                 viz_mask = np.round(
-                    np.linspace(0, len(viz_seq) - 1, args.viz_num_point)
+                    np.linspace(0, len(viz_seq) - 1, args.viz_num_point)  # evenly distribute the selected keypoints.
                 ).astype(int)
                 viz_seq = viz_seq[viz_mask]
 
+            # save the visualization as GIF
             viz_point_tracking_flow(
                 frames,
                 viz_seq,
